c6e8a3b4e6e825db3c6b8b65ce7d25c381a229e1
Meehoi <beam_baimon@hotmail.com>
2024-06-18 17:14:07 +0700

    lint fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,9 @@
 import express from 'express'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { createElement } from 'react'
 //@ts-expect-error Could not find a declaration file for module react-server-dom-webpack/server.browser.
 import { renderToReadableStream } from 'react-server-dom-webpack/server.browser'
 import App from '../client/app'
 import clientComponentMap from './_component'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const app = express()
 
